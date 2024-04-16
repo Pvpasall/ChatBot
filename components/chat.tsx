53b35184ed72f5dce4fa1986a26f9ebbfab786ae
Souleymane Sall <papa.sall243@gmail.com>
2024-04-16 14:59:57 +0200

    Initialize project

---
+++ components/chat.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useChat } from "ai/react";
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  return (
+    <div className="flex flex-col h-screen bg-black-100">
+      <div className="flex h-20 items-center justify-center bg-blue-500 p-4">
+        <h1 className="text-white text-2xl">Chatbot AI</h1>
+      </div>
+      <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+        {messages.map((m) => (
+          <div key={m.id} className="blackspace-pre-wrap">
+            {m.role === "user" ? "User: " : "AI: "}
+            {m.content}
+          </div>
+        ))}
+
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
