4b840cb2fe4041aa95aeac694744e0b7c49bec04
Souleymane Sall <papa.sall243@gmail.com>
2024-04-17 04:42:41 +0200

    can't hamdle redirect url. Check issues/1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Chat from "@/components/chat";
 import { HubspotSignInButton } from "@/components/login";
 
 export default function Page() {
+  // return <Chat />;
   return <HubspotSignInButton />;
 }
--- components/chat.tsx
+++ components/chat.tsx
@@ -4,19 +4,23 @@ import { useChat } from "ai/react";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
-export default async function Chat() {
-  const session = await getServerSession(authOptions);
+export default function Chat() {
+  //   const session = getServerSession(authOptions);
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div className="flex flex-col h-screen bg-black-100">
-      {session && <h1>Session:</h1>}
+    <div className="flex flex-col h-screen bg-black-100 overflow-y-auto">
       <div className="flex h-20 items-center justify-center bg-blue-500 p-4">
         <h1 className="text-white text-2xl">Chatbot AI</h1>
       </div>
       <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
         {messages.map((m) => (
-          <div key={m.id} className="blackspace-pre-wrap">
+          <div
+            key={m.id}
+            className={`blackspace-pre-wrap ${
+              m.role === "user" ? "text-blue-500" : "text-green-500"
+            }`}
+          >
             {m.role === "user" ? "User: " : "AI: "}
             {m.content}
           </div>
@@ -24,7 +28,7 @@ export default async function Chat() {
 
         <form onSubmit={handleSubmit}>
           <input
-            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black" // Added "text-black" to change input color to black
+            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
             value={input}
             placeholder="Say something..."
             onChange={handleInputChange}
